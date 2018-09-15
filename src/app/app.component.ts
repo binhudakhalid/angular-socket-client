761ceec7d8b57a210a33a51c620d9a152d07fe27
binhudakhalid <khalid.bin.huda@hotmail.com>
2018-09-15 13:08:20 +0500

    initial App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+(window as any).global = window;
+//import './globdal-shim.ts';
+import * as socketIo from 'socket.io-client';
+//(window as any).global = window;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'NgSocket';
+  
+
+  ngOnInit(): void {
+    (window as any).global = window;
+    const socket = socketIo('htt.://localhost:3000');
+
+
+    socket.on('hello', (data) => console.log(data));
+    //throw new Error("Method not implemented.");
+  }
+  
 }
